6f2d191902a5f3035b3880cf9114d95a0086fc16
kaif-lucifer <85788390+kaif-lucifer@users.noreply.github.com>
2023-12-06 13:23:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ let gameStats = {
     ties: 0,
 };
 
+// Function to show rules and icons in an alert
+function showRules() {
+    const rules = "Rock, Paper, Scissors Rules:\n\n" +
+                  "1. Rock crushes Scissors\n" +
+                  "2. Scissors cuts Paper\n" +
+                  "3. Paper covers Rock\n\n" +
+                  "Choose your move wisely!";
+
+    const icons = "Icons:\n\n" +
+                  "🪨🗿 - Rock\n" +
+                  "📄 - Paper\n" +
+                  "✂️ - Scissors";
+
+    const alertMessage = rules + "\n\n" + icons;
+
+    alert(alertMessage);
+}
+
+// Call the function to show the rules and icons
+showRules();
+
 function game(you,comp) {
     if (comp === you) {
        gameStats.ties++; 
@@ -12,7 +33,7 @@ function game(you,comp) {
                (comp === "Paper" && you === "Scissors") ||
                (comp === "Scissors" && you === "Rock")) {
         gameStats.wins++;
-        return "Congratulation!! You Won.";
+        return "Congratulation!! You Win.";
     } else {
         gameStats.losses++;
         return "Oh!! You Lose. ";
@@ -20,10 +41,41 @@ function game(you,comp) {
 }
 
 function computerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
-    const randomIndex = Math.floor(Math.random() * 3);
+    const choices = ["", "Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor((Math.random() * 3)+ 1);
+    console.log(randomIndex)
     return choices[randomIndex];
 }
+
+
+
+function playSound(result) {
+  const winSound = document.getElementById("winSound");
+  const loseSound = document.getElementById("loseSound");
+  const tieSound = document.getElementById("tieSound");
+
+  switch (result) {
+      case "Congratulation!! You Win.":
+        setTimeout(() => {
+          winSound.play();
+        }, 800);
+          break;
+      case "Oh!! You Lose. ":
+        setTimeout(() => {
+           loseSound.play();
+        }, 800);
+          break;
+      case "This game is a tie":
+        setTimeout(() => {
+          tieSound.play();
+        },800);
+          break;
+      default:
+          break;
+  }
+}
+
+
 
 let choices = document.querySelectorAll(".btn");
 let clickSounds = document.querySelectorAll(".clickSound");
@@ -31,14 +83,17 @@ for (let i = 0; i < choices.length; i++) {
     choices[i].addEventListener("click", function() {
        clickSounds[i].play();
         let you = this.id;
-        console.log("Your Choice:", you);
-        let comp = computerChoice();
-        console.log("Computer's Choice:", comp);
         document.getElementById("You").innerHTML = "You Chose: " + you;
-        document.getElementById("Comp").innerHTML = "Computer Chose: " + comp;
+      //  console.log("Your Choice:", you);
         
-      let result = game(you,comp);
-      console.log(result);
+        let comp = computerChoice();
+        //console.log("Computer's Choice:", comp);
+        document.getElementById("Comp").innerHTML = "Computer Chose: " + comp;
+        let result = game(you,comp);
+        playSound(result);
+      //loseSound.play();
+      //console.log(result);
+        
         document.getElementById("result").innerHTML = "Result: " + result;
         document.getElementById("player-score").innerHTML =  gameStats.wins;
         document.getElementById("computer-score").innerHTML = gameStats.losses;
